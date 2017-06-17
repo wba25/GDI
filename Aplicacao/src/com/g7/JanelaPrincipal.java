331184e74ff4179296aaf0b4502a12345a13b350
Ezequiel Filipe C Reis <ezequiel_reis_10@hotmail.com>
2017-06-17 14:13:45 -0300

    Pasta para a aplicação
    adicionei uma pasta para fazer o gui

---
+++ Aplicacao/src/com/g7/JanelaPrincipal.java
@@ -0,0 +1,32 @@
+package com.g7;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Ezequiel Reis on 16/06/2017.
+ */
+public class JanelaPrincipal {
+    private JPanel JanelaPrincipal;
+    private JButton novoPacienteButton;
+    private JButton novoMédicoButton;
+    private JButton marcarConsultaButton;
+
+    public JanelaPrincipal() {
+        novoPacienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(NovoPaciente);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JanelaPrincipal");
+        frame.setContentPane(new JanelaPrincipal().JanelaPrincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ Aplicacao/src/com/g7/NovoPaciente.java
@@ -0,0 +1,11 @@
+package com.g7;
+
+import javax.swing.*;
+
+/**
+ * Created by Ezequiel Reis on 16/06/2017.
+ */
+public class NovoPaciente {
+    private JTextField textField1;
+    private JTextField textField2;
+}
