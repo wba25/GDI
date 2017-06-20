ff81c6840679a156c250dada6897b488a3a09b95
Carolina <carolinareisilva@gmail.com>
2017-06-20 19:17:09 -0300

    Parte Carol

--- Lista OR XML/q5.sql
+++ Lista OR XML/q5.sql
@@ -1,28 +1,37 @@
 /*Crie um procedure para o tipo laboratório que imprime o id, o nome e o(s) telefone(s) do laboratório se
 neste ocorreu um agendamento no mês passado como parâmetro, caso contrário levante uma exceção.*/
 
-CREATE OR REPLACE PROCEDURE p_agendamento(data_hora_in TIMESTAMP) IS
+CREATE OR REPLACE PROCEDURE p_agendamento(data_horai IN TIMESTAMP) IS
 id1 NUMBER(3);
 nome1 VARCHAR2(200);
 telefone1 VARCHAR2(20);
 vEXCEPTION EXCEPTION;		
 
+CURSOR cr_telefone IS SELECT * FROM tb_fone;
+v_telefone cr_telefone%rowtype;
+
 BEGIN
-IF(month(data_hora_in) = month(current_timestamp - 1)) THEN
-SELECT l.id INTO id1, 
-	   l.nome INTO nome1,
-	   l.laboratorio_telefone INTO telefone1
-FROM tp_laboratorio l, tp_agenda_exame a
-WHERE
-	   a.laboratorio = l.id AND
-	   a.data_hora = data_hora_in;
-dbms_output.put_line(id1);
-dbms_output.put_line(nome1);
-dbms_output.put_line(telefone1);	   
-ELSE
-EXCEPTION
-		WHEN vEXCEPTION THEN
-			RAISE_APPLICATION_ERROR('-20999','ATENÇÃO! Mês inválido. Informe uma data do mês passado.', FALSE);
-END IF;
+	OPEN cr_telefone;
+	LOOP
+	FETCH cr_telefone INTO v_telefone
+	EXIT WHEN cr_telefone
+
+BEGIN
+	IF EXTRACT(MONTH FROM data_horai) = (EXTRACT(MONTH FROM CURRENT_TIMESTAMP) - 1) THEN
+		SELECT l.id INTO id1, 
+			   l.nome INTO nome1,
+			   f.numero INTO telefone1
+		FROM tb_laboratorio l, tb_agenda_exame a, tp_fone f
+		WHERE
+			   a.laboratorio = l.id AND
+			   a.data_hora = data_horai;
+		dbms_output.put_line(id1);
+		dbms_output.put_line(nome1);
+		dbms_output.put_line(telefone1);	   
+	ELSE
+		EXCEPTION
+				WHEN vEXCEPTION THEN
+					RAISE_APPLICATION_ERROR('-20999','ATENÇÃO! Mês inválido. Informe uma data do mês passado.', FALSE);
+	END IF;
 END p_agendamento;
 /
